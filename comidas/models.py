acb5e0583e2e855e0fbe40d2e5b3a7f554700752
Nicolas Sobral Cruz <nicolasunif@gmail.com>
2024-11-07 20:25:37 -0300

    Criação do modelo Post e registro do administrador do Django. Superusuário recém criado também

--- comidas/models.py
+++ comidas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=255)
+    conteudo = models.TextField()
+    data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.titulo} ({self.data})'
