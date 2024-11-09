f4e83b55e5c6cc34bc59acc0d149f90d2e75e0ef
Nicolas Sobral Cruz <nicolasunif@gmail.com>
2024-11-09 17:06:30 -0300

    adição do modelo de comentarios bem como a sua exibição noas páginas de detail dos posts

--- comidas/models.py
+++ comidas/models.py
@@ -9,3 +9,11 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.titulo} ({self.data})'
+    
+class Comment(models.Model):
+    autor= models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    texto=models.CharField(max_length=1000)
+    data = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.texto} - {self.autor.username}'
